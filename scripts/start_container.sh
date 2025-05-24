f3eb1ecdb46d90bfbabb5f3668e8ee656d583730
shruthi-beep <shruthi.manga27@gmail.com>
2025-05-24 18:26:05 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+
+docker push shruthimanga/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 shruthimanga/simple-python-flask-app
